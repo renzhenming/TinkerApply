c19b0a91b1f6f84a0d43e9991aae84c341b5ab19
renzhenming <1565766177@qq.com>
2018-09-10 07:28:02 +0800

    test

--- app/src/main/java/com/rzm/tinkerapply/MainActivity.java
+++ app/src/main/java/com/rzm/tinkerapply/MainActivity.java
@@ -1,7 +1,17 @@
 package com.rzm.tinkerapply;
 
+import android.Manifest;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView tvBug = findViewById(R.id.tv_bug);
+        tvBug.setText("---------修改了内容");
+        requestPermission();
+    }
+
+    public void requestPermission(){
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        TinkerInstaller.onReceiveUpgradePatch(getApplication().getApplicationContext(),
+                Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk");
+        Toast.makeText(getApplicationContext(),"修复",Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/rzm/tinkerapply/SampleApplicationLike.java
@@ -0,0 +1,45 @@
+package com.rzm.tinkerapply;
+
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.multidex.MultiDex;
+
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.lib.tinker.Tinker;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.app.DefaultApplicationLike;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+@DefaultLifeCycle(flags = ShareConstants.TINKER_ENABLE_ALL,application = "com.rzm.tinkerapply.SampleApplication"
+)
+public class SampleApplicationLike extends DefaultApplicationLike {
+    public SampleApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
+    }
+    /**
+     * install multiDex before install tinker
+     * so we don't need to put the tinker lib classes in the main dex
+     *
+     * @param base
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        //you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+        //installTinker after load multiDex
+        //or you can put com.tencent.tinker.** to main dex
+        TinkerInstaller.install(this);
+        Tinker tinker = Tinker.with(getApplication());
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks callback) {
+        getApplication().registerActivityLifecycleCallbacks(callback);
+    }
+
+}
